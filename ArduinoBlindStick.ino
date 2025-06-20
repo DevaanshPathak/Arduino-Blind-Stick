ef1007161cb299ea011c717b01a950d3a77bd0dc
Devaansh Pathak <devaanshpathak08@gmail.com>
2025-06-20 18:28:04 +0530

    Update ArduinoBlindStick.ino

--- ArduinoBlindStick.ino
+++ ArduinoBlindStick.ino
@@ -1,20 +1,20 @@
 // defines pins numbers
-const int trigPin = 9;
-const int echoPin = 10;
-const int buzzer = 11;
-const int ledPin = 13;
+const int trigPin = 9; // HC-SR04 Trig Pin Defination
+const int echoPin = 10; // HC-SR04 Echo Pin Defination
+const int buzzer = 11; // Buzzer Pin Defination
+const int ledPin = 13; // Led Pin Defination
 
 // defines variables
-long duration;
-int distance;
-int safetyDistance;
+long duration; // Time Variable
+int distance; // Distance read by sensor
+int safetyDistance; // Safety Distance if distance less than safetyDistance then buzzer and led goes off
 
 
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-pinMode(buzzer, OUTPUT);
-pinMode(ledPin, OUTPUT);
+pinMode(buzzer, OUTPUT); // Sets Buzzer as output
+pinMode(ledPin, OUTPUT); // Sets LED as output
 Serial.begin(9600); // Starts the serial communication
 }
 
@@ -36,7 +36,9 @@ duration = pulseIn(echoPin, HIGH);
 distance= duration*0.034/2;
 
 safetyDistance = distance;
-if (safetyDistance <= 25){
+
+// Distance and Buzzer Logic
+if (safetyDistance <= 25){ 
   digitalWrite(buzzer, HIGH);
   digitalWrite(ledPin, HIGH);
 }
